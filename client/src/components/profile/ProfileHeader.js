78143e25c56a971e25c2fb1c843c276e6dbc275b
Matt Dobson <matt@mattdobson.me>
2018-05-23 12:23:33 +0100

    Profile Modals

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import {
   followUser,
   unfollowUser,
@@ -46,68 +47,194 @@ class ProfileHeader extends Component {
     const { posts } = this.props;
 
     return (
-      <div className="movieHeader">
-        <div className="jumbotron jumbotron-fluid bg-navy text-white box-shadow mb-0 border-bottom-neon">
-          <div className="container text-center">
-            <div className="col-md-8 m-auto">
-              <img
-                src={profile.user.avatar}
-                alt=""
-                className="rounded border-orange box-shadow"
-              />
-              <div className="profile-info">
-                <h1 className="text-orange">
-                  <strong>{profile.username}</strong>
-                </h1>
-                {profile.bio ? <p>{profile.bio}</p> : null}
-                {profile.location ? (
-                  <p className="mb-0">
-                    <strong className="text-orange">Location</strong>{" "}
-                    {profile.location}
-                  </p>
+      <span>
+        <div className="movieHeader">
+          <div className="jumbotron jumbotron-fluid bg-navy text-white box-shadow mb-0 border-bottom-neon">
+            <div className="container text-center">
+              <div className="col-md-8 m-auto">
+                <img
+                  src={profile.user.avatar}
+                  alt=""
+                  className="rounded border-orange box-shadow"
+                />
+                <div className="profile-info">
+                  <h1 className="text-orange">
+                    <strong>{profile.username}</strong>
+                  </h1>
+                  {profile.bio ? <p>{profile.bio}</p> : null}
+                  {profile.location ? (
+                    <p className="mb-0">
+                      <strong className="text-orange">Location</strong>{" "}
+                      {profile.location}
+                    </p>
+                  ) : null}
+                </div>
+
+                {user.username !== profile.user.username && isAuthenticated ? (
+                  this.findUserFollowing(profile.followers) ? (
+                    <div
+                      className="btn btn-outline-neon mt-3"
+                      onClick={this.onClick}
+                    >
+                      UNFOLLOW
+                    </div>
+                  ) : (
+                    <div
+                      className="btn btn-outline-neon mt-3"
+                      onClick={this.onClick}
+                    >
+                      FOLLOW
+                    </div>
+                  )
                 ) : null}
               </div>
+            </div>
+          </div>
 
-              {user.username !== profile.user.username && isAuthenticated ? (
-                this.findUserFollowing(profile.followers) ? (
-                  <div
-                    className="btn btn-outline-neon mt-3"
-                    onClick={this.onClick}
+          <div className="ratings p-3 box-shadow text-white bg-navy border-bottom-neon">
+            <div className="container text-center">
+              <div className="row">
+                <div className="col-md-4 border-right">
+                  <strong>{posts ? posts.length : null}</strong>
+                  <p className="mb-0">Posts</p>
+                </div>
+                <div className="col-md-4 border-right">
+                  <a
+                    href="#followingModal"
+                    data-toggle="modal"
+                    className="text-white"
                   >
-                    UNFOLLOW
-                  </div>
-                ) : (
-                  <div
-                    className="btn btn-outline-neon mt-3"
-                    onClick={this.onClick}
+                    <strong>{profile.following.length}</strong>
+                    <p className="mb-0">Following</p>
+                  </a>
+                </div>
+                <div className="col-md-4">
+                  <a
+                    href="#followersModal"
+                    data-toggle="modal"
+                    className="text-white"
                   >
-                    FOLLOW
-                  </div>
-                )
-              ) : null}
+                    <strong className="m">{profile.followers.length}</strong>
+                    <p className="mb-0 mt-0">Followers</p>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="ratings p-3 box-shadow text-white bg-navy border-bottom-neon">
-          <div className="container text-center">
-            <div className="row">
-              <div className="col-md-4 border-right">
-                <strong>{posts ? posts.length : null}</strong>
-                <p className="mb-0">Posts</p>
+        {/* Following Modal */}
+        <div
+          className="modal fade bg-navy-50"
+          id="followingModal"
+          role="dialog"
+          aria-labelledby="exampleModalCenterTitle"
+          aria-hidden="true"
+          data-backdrop="false"
+        >
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content bg-navy text-white border-bottom-neon">
+              <div className="modal-header border-bottom-neon">
+                <h5
+                  className="modal-title font-weight-bold text-neon"
+                  id="exampleModalLongTitle"
+                >
+                  Following
+                </h5>
+                <button
+                  type="button"
+                  className="close text-white"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <i className="fas fa-times" />
+                </button>
               </div>
-              <div className="col-md-4 border-right">
-                <strong>{profile.following.length}</strong>
-                <p className="mb-0">Following</p>
-              </div>
-              <div className="col-md-4">
-                <strong className="m">{profile.followers.length}</strong>
-                <p className="mb-0 mt-0">Followers</p>
+              <div className="modal-body py-0">
+                {profile.following.map(user => (
+                  <div key={user._id}>
+                    <div className="followerItem d-flex py-3">
+                      <img
+                        src={user.userAvatar}
+                        alt=""
+                        width="64px"
+                        height="64px"
+                        className="rounded"
+                      />
+                      <p className="ml-3 p-0 my-auto lh-100">{user.username}</p>
+
+                      <div className="ml-auto my-auto">
+                        <Link
+                          to={`/profile/${user.username}`}
+                          className="btn btn-outline-neon"
+                        >
+                          View Profile
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
         </div>
-      </div>
+
+        {/* Follower Modal */}
+        <div
+          className="modal fade bg-navy-50"
+          id="followersModal"
+          role="dialog"
+          aria-labelledby="exampleModalCenterTitle"
+          aria-hidden="true"
+          data-backdrop="false"
+        >
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content bg-navy text-white border-bottom-neon">
+              <div className="modal-header border-bottom-neon">
+                <h5
+                  className="modal-title font-weight-bold text-neon"
+                  id="exampleModalLongTitle"
+                >
+                  Followers
+                </h5>
+                <button
+                  type="button"
+                  className="close text-white"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <i className="fas fa-times" />
+                </button>
+              </div>
+              <div className="modal-body py-0">
+                {profile.followers.map(user => (
+                  <div key={user._id}>
+                    <div className="followerItem d-flex py-3">
+                      <img
+                        src={user.userAvatar}
+                        alt=""
+                        width="64px"
+                        height="64px"
+                        className="rounded"
+                      />
+                      <p className="ml-3 p-0 my-auto lh-100">{user.username}</p>
+
+                      <div className="ml-auto my-auto">
+                        <Link
+                          to={`/profile/${user.username}`}
+                          className="btn btn-outline-neon"
+                        >
+                          View Profile
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </span>
     );
   }
 }
