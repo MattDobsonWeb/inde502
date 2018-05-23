63d3e8b116a409880d1dc7cfbe27c855fa5fb336
Matt Dobson <matt@mattdobson.me>
2018-05-23 16:49:20 +0100

    Production and Dev Redux Stores

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,5 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers";
-
-const initialState = {};
-
-const middleware = [thunk];
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const enhancer = composeEnhancers(
-  applyMiddleware(...middleware)
-  // other store enhancers if any
-);
-
-const store = createStore(rootReducer, initialState, enhancer);
-
-export default store;
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./store_prod");
+} else {
+  module.exports = require("./store_dev");
+}
